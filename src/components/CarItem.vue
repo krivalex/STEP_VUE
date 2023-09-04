6558c384af6faa1292e9f48756abe1f0194fe6b8
Алексей Кривобородов <@itstep.loc>
2023-09-04 21:49:06 +0600

    working

--- src/components/CarItem.vue
+++ src/components/CarItem.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { defineProps, computed } from 'vue'
+import Card from 'primevue/card'
+import Chip from 'primevue/chip'
 
 const props = defineProps({
   car: {
@@ -25,34 +27,24 @@ function changeColor(color) {
 </script>
 
 <template>
-  <div class="car">
-    <img :src="carRemake.image" alt="car" />
-    <h2>{{ carRemake.brand }}</h2>
-    <p>Цена: {{ carRemake.price }}</p>
-    <p>Год выпуска: {{ carRemake.year }}</p>
-    <p>Объем двигателя: {{ carRemake.volume }}</p>
-    <p :style="`color: ${carRemake.color}`">Цвет: {{ carRemake.color }}</p>
-    <div class="chipes">
-      <span v-if="Number(carRemake.price.slice(0, -1)) > 3000000">Дорогой </span>
-      <span v-else-if="Number(carRemake.year) <= 1960">Старый</span>
-      <span v-else>Скучный</span>
-      <span v-if="changeColor(carRemake.color)">Конченный цвет</span>
-    </div>
-  </div>
+  <Card>
+    <template #header>
+      <img :src="carRemake.image" alt="car" class="car-image" />
+    </template>
+    <template #title> {{ carRemake.brand }} </template>
+    <template #content>
+      <p>Цена: {{ carRemake.price }}</p>
+      <p>Год выпуска: {{ carRemake.year }}</p>
+      <p>Объем двигателя: {{ carRemake.volume }}</p>
+      <p :style="`color: ${carRemake.color}`">Цвет: {{ carRemake.color }}</p>
+    </template>
+    <template #footer>
+      <Chip v-if="Number(carRemake.price.slice(0, -1)) > 1000000" label="Дорогой" icon="pi pi-apple" />
+      <Chip icon="pi pi-history" label="Старый" v-else-if="Number(carRemake.year) <= 1960" />
+      <Chip icon="pi pi-briefcase" label="Скучный" v-else />
+      <Chip icon="pi pi-eye-slash" label="Конченный цвет" v-if="changeColor(carRemake.color)" />
+    </template>
+  </Card>
 </template>
 
-<style scoped>
-.chipes {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: column;
-}
-
-.chipes span {
-  margin: 5px;
-  padding: 5px;
-  border: 1px solid black;
-  border-radius: 5px;
-}
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import './assets/main.css'
 
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,5 +12,6 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
